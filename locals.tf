1a01a576fe854c05f47b66a541d1b4b5d64f040d
Tim Morgan <bitbucket@myemailbox.cloud>
2020-01-31 17:08:48 -0600

    Fixed finding container name

--- locals.tf
+++ locals.tf
@@ -1,7 +1,10 @@
 # locals
 
 locals {
-    container_definition = jsondecode(var.container_definition)
+    # Will only get the first container definition, if more than one exists
+    container_definition = jsondecode(var.container_definition)[0]
+    # This does not work. Says local.container_definition is a 
+    #    tuple of 1 element and not a map.
     container_name = lookup(local.container_definition, "name", "error")
 }
 
