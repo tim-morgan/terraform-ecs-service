df85da6ae07f689cec60be77d8f933aea12f2251
Tim Morgan <bitbucket@myemailbox.cloud>
2020-01-30 21:45:44 -0600

    Fixed syntax errors

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 # locals
 
 locals {
-    container_definition = jsondecode(var.ontainer_definition)
-    container_name = lookup(container_definition, "name", "error")
+    container_definition = jsondecode(var.container_definition)
+    container_name = lookup(var.container_definition, "name", "error")
 }
 
